b7de890f180ae5e51fb4d0ce6fbee0ec83dfd876
YinaePark <dlsoso1@unist.ac.kr>
2023-11-17 21:29:44 +0900

    first commit

---
+++ unistudy/src/main/java/unistudy/unistudy/UnistudyApplication.java
@@ -0,0 +1,13 @@
+package unistudy.unistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnistudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnistudyApplication.class, args);
+	}
+
+}
---
+++ unistudy/src/test/java/unistudy/unistudy/UnistudyApplicationTests.java
@@ -0,0 +1,13 @@
+package unistudy.unistudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnistudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
