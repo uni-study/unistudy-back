829aa22c0edd0e18c005379bc9f0794600e226d5
YinaePark <dlsoso1@unist.ac.kr>
2023-11-17 22:25:14 +0900

    feat: user domain

---
+++ unistudy/src/main/java/unistudy/unistudy/domain/Post.java
@@ -0,0 +1,78 @@
+package unistudy.unistudy.domain;
+
+import java.util.Date;
+
+public class Post {
+    private int id;
+    private int writerId; // Foreign Key referencing User table
+    private int studyGroupId; // Foreign Key referencing Studygroup table
+    private String title;
+    private String mainText;
+    private Date postedAt;
+    private Date updatedAt;
+    private Date expiredAt;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setWriterId(int writerId) {
+        this.writerId = writerId;
+    }
+
+    public int getWriterId() {
+        return writerId;
+    }
+
+    public void setStudyGroupId(int studyGroupId) {
+        this.studyGroupId = studyGroupId;
+    }
+
+    public int getStudyGroupId() {
+        return studyGroupId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setMainText(String mainText) {
+        this.mainText = mainText;
+    }
+
+    public String getMainText() {
+        return mainText;
+    }
+
+    public void setPostedAt(Date postedAt) {
+        this.postedAt = postedAt;
+    }
+
+    public Date getPostedAt() {
+        return postedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setExpiredAt(Date expiredAt) {
+        this.expiredAt = expiredAt;
+    }
+
+    public Date getExpiredAt() {
+        return expiredAt;
+    }
+}
