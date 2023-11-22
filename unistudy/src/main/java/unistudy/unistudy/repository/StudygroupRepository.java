869fcef871e8f103deaf2165af58da594de4cb49
YinaePark <dlsoso1@unist.ac.kr>
2023-11-22 13:41:16 +0900

    feat: Studygroup 생성, 삭제, 수정, 검색 필터링

--- unistudy/src/main/java/unistudy/unistudy/repository/StudygroupRepository.java
+++ unistudy/src/main/java/unistudy/unistudy/repository/StudygroupRepository.java
@@ -4,6 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import unistudy.unistudy.domain.Studygroup;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StudygroupRepository extends JpaRepository<Studygroup, Integer> {
+
+    @Override
+    Optional<Studygroup> findById(Integer integer);
+    List<Studygroup> findByLeaderId(Integer leaderId);
+    List<Studygroup> findByNameContaining(String name);
+    List<Studygroup> findByDescriptionContaining(String description);
+    List<Studygroup> findByCurrentState(Integer state);
+    List<Studygroup> findByContactContaining(String contact);
+    List<Studygroup> findByDepartment(Integer department);
+
+    List<Studygroup> findByRecruitmentDeadlineBetween(Date startDate, Date endDate);
 }
